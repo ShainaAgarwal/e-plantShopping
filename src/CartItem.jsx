5480b2fa790961054d0c43f0f2d9a8a4d234f4ae
Shaina Agarwal <shaina.agarwal2708@gmail.com>
2026-03-11 12:46:46 -0400

    Update cartitem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -56,15 +56,65 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleCheckout = () => {
-  if (cart.length === 0) {
-    alert("Your cart is empty!! Add some plants before checkout.");
-  } else {
-    alert(`Proceeding to checkout. Total amount: $${calculateTotalAmount()}`);
-    // Future: redirect to a payment page or summary page
-  }
+    alert("Checkout Coming Soon!");
 };
 
   return (
+    
+    <div>
+    <div className="navbar" style={styleObj}>
+        <div className="tag">
+            <div className="luxury">
+                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
+                <a href="/" onClick={(e) => handleHomeClick(e)}>
+                    <div>
+                        <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
+                        <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+        <div style={styleObjUl}>
+
+  <div>
+    <a href="#" onClick={(e) => handleHomeClick(e)} style={styleA}>
+      Home
+    </a>
+  </div>
+
+  <div>
+    <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>
+      Plants
+    </a>
+  </div>
+
+  <div>
+    <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+      <h1 className="cart">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" height="68" width="68">
+          <rect width="156" height="156" fill="none"></rect>
+          <circle cx="80" cy="216" r="12"></circle>
+          <circle cx="184" cy="216" r="12"></circle>
+          <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8"
+            fill="none"
+            stroke="#faf9f9"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+          ></path>
+        </svg>
+
+        <span style={{ marginLeft: "10px" }}>
+          {calculateTotalQuantity()}
+        </span>
+      </h1>
+    </a>
+  </div>
+
+</div>
+    </div>
+</div>
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
       <div>
